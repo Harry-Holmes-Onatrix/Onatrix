cd5a1f35fe78ca00f4f099797bc3493ee1178c98
Harry Holmes <harryholmesse@gmail.com>
2025-10-30 12:27:16 +0100

    added base structure

--- Onatrix/umbraco/models/ServiceSettings.generated.cs
+++ Onatrix/umbraco/models/ServiceSettings.generated.cs
@@ -55,7 +55,7 @@ namespace Umbraco.Cms.Web.Common.PublishedModels
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.2.0+4eae48e")]
 		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
 		[ImplementPropertyType("serviceBgColour")]
-		public virtual string ServiceBgColour => this.Value<string>(_publishedValueFallback, "serviceBgColour");
+		public virtual global::Umbraco.Cms.Core.PropertyEditors.ValueConverters.ColorPickerValueConverter.PickedColor ServiceBgColour => this.Value<global::Umbraco.Cms.Core.PropertyEditors.ValueConverters.ColorPickerValueConverter.PickedColor>(_publishedValueFallback, "serviceBgColour");
 
 		///<summary>
 		/// Show Label
