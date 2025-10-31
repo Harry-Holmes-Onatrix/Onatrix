30f084b2450b6b352a9c085360d8d4705807ab00
Harry Holmes <harryholmesse@gmail.com>
2025-10-31 12:55:48 +0100

    added form result to controller

--- Onatrix/Controllers/FormController.cs
+++ Onatrix/Controllers/FormController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Onatrix.Services;
 using Onatrix.ViewModels;
 using Umbraco.Cms.Core.Cache;
 using Umbraco.Cms.Core.Logging;
@@ -10,17 +11,22 @@ using Umbraco.Cms.Web.Website.Controllers;
 
 namespace Onatrix.Controllers
 {
-    public class FormController : SurfaceController
+    public class FormController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider, FormSubmissionsService formSubmissionsService) : SurfaceController(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
     {
-        public FormController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
-        {
-        }
+        private readonly FormSubmissionsService _formSubmissionsService = formSubmissionsService;
 
         public IActionResult HandleCallbackForm(CallbackFormViewModel model)
         {
             if (!ModelState.IsValid)
             {
                 return CurrentUmbracoPage();
+            }
+
+            var result = _formSubmissionsService.SaveCallbackRequest(model);
+
+            if (!result)
+            {
+                return RedirectToCurrentUmbracoPage();
             }
 
 
