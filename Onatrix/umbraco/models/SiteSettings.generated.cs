2744487d6338d1bc943b7814c7aa1c8bff86285b
Harry Holmes <harryholmesse@gmail.com>
2025-11-05 14:56:41 +0100

    created and added content into the brands section on Umrbaco

--- Onatrix/umbraco/models/SiteSettings.generated.cs
+++ Onatrix/umbraco/models/SiteSettings.generated.cs
@@ -50,6 +50,14 @@ namespace Umbraco.Cms.Web.Common.PublishedModels
 		// properties
 
 		///<summary>
+		/// Brand List
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.2.0+4eae48e")]
+		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
+		[ImplementPropertyType("brandList")]
+		public virtual global::Umbraco.Cms.Core.Models.Blocks.BlockListModel BrandList => this.Value<global::Umbraco.Cms.Core.Models.Blocks.BlockListModel>(_publishedValueFallback, "brandList");
+
+		///<summary>
 		/// Address
 		///</summary>
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.2.0+4eae48e")]
