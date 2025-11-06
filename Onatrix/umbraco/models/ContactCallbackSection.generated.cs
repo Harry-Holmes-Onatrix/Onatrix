05d35e3f9cb8151f02da4fab988a2073cf54d05d
Harry Holmes <harryholmesse@gmail.com>
2025-11-06 10:54:41 +0100

    rendered the content on the contact page

--- Onatrix/umbraco/models/ContactCallbackSection.generated.cs
+++ Onatrix/umbraco/models/ContactCallbackSection.generated.cs
@@ -50,6 +50,14 @@ namespace Umbraco.Cms.Web.Common.PublishedModels
 		// properties
 
 		///<summary>
+		/// Background Image
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.2.0+4eae48e")]
+		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
+		[ImplementPropertyType("contactCallbackBackgroundImage")]
+		public virtual global::Umbraco.Cms.Core.Models.MediaWithCrops ContactCallbackBackgroundImage => this.Value<global::Umbraco.Cms.Core.Models.MediaWithCrops>(_publishedValueFallback, "contactCallbackBackgroundImage");
+
+		///<summary>
 		/// Form Body
 		///</summary>
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.2.0+4eae48e")]
