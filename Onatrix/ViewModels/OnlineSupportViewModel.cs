55cd07327ae29b4bb8decde595fd2dfa30f0c823
Harry Holmes <harryholmesse@gmail.com>
2025-11-06 13:20:41 +0100

    added a view model

---
+++ Onatrix/ViewModels/OnlineSupportViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Onatrix.ViewModels;
+
+public class OnlineSupportViewModel
+{
+    [Required(ErrorMessage = "Email is required")]
+    [Display(Name = "Email address")]
+    [RegularExpression(@"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$", ErrorMessage = "Invalid email address")]
+    public string Email { get; set; } = string.Empty;
+}
